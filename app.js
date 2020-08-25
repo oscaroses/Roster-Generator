e41b9eb695bf368d3fc620600e739aa334363fe1
PeeWee <oscar.oses.1127@gmail.com>
2020-08-24 20:44:35 -0400

    added comments to app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Required files and modules
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -10,33 +11,55 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+//Array that will hold the objects we create with the prompts.
 var employees = []
 
-function createManager() {
+
+//Inital function to prompt the user to enter Manager info. This will then call the function to let the user input more team members.
+function addManager() {
     inquirer.prompt([
         {
             type: "input",
-            name: "man"
-
+            message: "What is this team managers name?",
+            name: "manName"
+        },
+        {
+            type: "input",
+            message: "Assign an ID number for the manager",
+            name: "manID"
+        },
+        {
+            type: "input",
+            message: "Provide an email for the manager",
+            name: "manEmail"
+        },
+        {
+            type: "input",
+            message: "What is your office number?",
+            name: "manOfficeNumber"
         }
-    ])
+    ]).then(userInput => {
+        console.log(userInput);
+
+        const manager = new Manager(userInput.manName, userInput.manID, userInput.manEmail, userInput.manOfficeNumber)
+
+        employees.push(manager)
+        createTeam();
+    })
 }
 
 
-
+//Function used to create rest of the team members. Using a switch to call specified team member functions. When no more members is selected, the html file is rendered then written.
 function createTeam() {
     inquirer.prompt([
         {
             type: "list",
             name: "empType",
-            message: "What type of team member are you?",
-            choices: ["Manager", "Engineer", "Intern", "No more members"]
+            message: "Add a team member:",
+            choices: ["Engineer", "Intern", "No more members"]
         }])
         .then(userInput => {
             switch (userInput.empType) {
-                case "Manager":
-                    addManager();
-                    break;
 
                 case "Engineer":
                     addEngineer();
@@ -55,39 +78,7 @@ function createTeam() {
                     break
             }
         })
-
-    function addManager() {
-        inquirer.prompt([
-            {
-                type: "input",
-                message: "What is this team managers name?",
-                name: "manName"
-            },
-            {
-                type: "input",
-                message: "Assign an ID number for the manager",
-                name: "manID"
-            },
-            {
-                type: "input",
-                message: "Provide an email for the manager",
-                name: "manEmail"
-            },
-            {
-                type: "input",
-                message: "What is your office number?",
-                name: "manOfficeNumber"
-            }
-        ]).then(userInput => {
-            console.log(userInput);
-
-            const manager = new Manager(userInput.manName, userInput.manID, userInput.manEmail, userInput.manOfficeNumber)
-
-            employees.push(manager)
-            createTeam();
-        })
-    }
-
+    // These two functions will call the team member function after they input member info.
     function addEngineer() {
         inquirer.prompt([
             {
@@ -153,4 +144,4 @@ function createTeam() {
     }
 }
 
-createTeam()
+addManager()
